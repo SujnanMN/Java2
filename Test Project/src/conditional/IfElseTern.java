f18515102d28863ce160427764a6f2a5ffd49fa0
smn20 <smn20@DESKTOP-F63J3PT>
2021-10-19 00:26:45 -0700

    Commit 12:56

---
+++ Test Project/src/conditional/IfElseTern.java
@@ -0,0 +1,10 @@
+package conditional;
+
+public class IfElseTern {
+	public static void main(String[] args) {    
+	    int number=26;    
+	    String output = (number % 2 == 0) ? "even number":"odd number";    
+	    System.out.println(output);  
+	}    
+
+}
---
+++ Test Project/src/conditional/Leap.java
@@ -0,0 +1,14 @@
+package conditional;
+
+public class Leap {
+	public static void main(String[] args) {    
+	    int year = 2020;    
+	    if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){  
+	        System.out.println("LEAP YEAR");  
+	    }  
+	    else{  
+	        System.out.println("COMMON YEAR");  
+	    }  
+	}   
+
+}
