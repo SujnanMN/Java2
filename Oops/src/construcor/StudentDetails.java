85b43597c1aeb32cea6f4ab566cda419e51ce0f3
smn20 <smn20@IN-PG02M1BA>
2021-10-20 11:43:22 +0530

    construct

--- Oops/src/construcor/StudentDetails.java
+++ Oops/src/construcor/StudentDetails.java
@@ -1,5 +1,19 @@
 package construcor;
 
 public class StudentDetails {
-
+	int age;
+	String name;
+	//default cons
+	public StudentDetails()
+	//cons with one para
+	public StudentDetails(int age)
+	{
+		this.age=age;
+	}
+	//cons with two para
+	public StudentDetails(int age, String name)
+	{
+		this.age=age;
+		this.name=name;
+	}
 }
---
+++ Oops/src/construcor/Test.java
@@ -0,0 +1,9 @@
+package construcor;
+
+public class Test {
+	public static void main(String[] args) {
+		StudentDetails ram = new StudentDetails();
+		StudentDetails me = new StudentDetails(22, "Me");
+		StudentDetails raman = new StudentDetails(25);
+	}
+}
