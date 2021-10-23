f3180faf5e13a1e6eb9c8dc410b32f91c99b495c
smn20 <smn20@IN-PG02M1BA>
2021-10-24 01:38:34 +0530

    24/10 1:38 am

---
+++ FirstWeekAssign/src/stringpractise/SubstringComp.java
@@ -0,0 +1,28 @@
+package stringpractise;
+
+import java.util.Scanner;
+
+public class SubstringComp {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s=scan.nextLine();
+		int a=scan.nextInt();
+		int n=s.length();
+		String b=s.substring(0,a);
+		String min;
+		String max=min=s.substring(0,a);
+		for (int i=a;i<=n-a; i+=a)
+		{
+			String c=s.substring(i,i+a);
+			if (c.compareTo(max)>0)
+			max=c;
+			if (c.compareTo(min)<0)
+			min=c;
+		}
+		System.out.println(1+min+" "+2+max);
+		scan.close();
+
+	}
+
+}
