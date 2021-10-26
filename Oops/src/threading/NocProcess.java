ce466e5f273a6500e6ccef45e9c4911df3a058ea
smn20 <smn20@IN-PG02M1BA>
2021-10-26 13:02:02 +0530

    queue

---
+++ Oops/src/threading/NocProcess.java
@@ -0,0 +1,30 @@
+package threading;
+
+public class NocProcess {
+
+	public static void main(String[] args) {
+		//oldSequentialWay();
+
+		FeeThread feeThread = new FeeThread();
+		feeThread.start();
+
+
+	}
+
+	private static void oldSequentialWay() {
+		try {
+			//fee
+			Thread.sleep(500);
+			System.out.println("got the fee no due stamp");
+			//Hotel
+			Thread.sleep(500);
+			System.out.println("got the fee no due stamp");
+			//transport
+			Thread.sleep(500);
+			System.out.println("got the fee no due stamp");
+			//library
+			Thread.sleep(500);
+			System.out.println("got the fee no due stamp");
+		}
+		catch (Exception e) {
+		}
